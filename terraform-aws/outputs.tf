2d661a16851aaa7b2d3f79869a956e9200c032fb
Aleksandr Kostyrev <kostyrevaa@gmail.com>
2018-01-31 13:16:24 +0300

    Add clients_elb_zone_id output to be able to create dns alias

--- terraform-aws/outputs.tf
+++ terraform-aws/outputs.tf
@@ -1,3 +1,6 @@
-output "clients_dns" {
+output "clients_elb_dns_name" {
   value = "${aws_elb.es_client_lb.dns_name}"
+}
+output "clients_elb_zone_id" {
+  value = "${aws_elb.es_client_lb.zone_id}"
 }
