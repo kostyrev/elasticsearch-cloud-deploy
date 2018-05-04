8040ea560c6c10a2d0baf0eefef817658e81f165
Konstantin Chugalinskiy <k.chugalinskiy@fasten.com>
2018-05-04 12:42:22 +0300

    Fix indentation

--- terraform-azure/lb.tf
+++ terraform-azure/lb.tf
@@ -7,15 +7,15 @@ resource "azurerm_public_ip" "clients" {
 }
 
 resource "azurerm_lb" "clients" {
-//  count = "${var.associate_public_ip == "true" ? "0" : "1"}"
+  //  count = "${var.associate_public_ip == "true" ? "0" : "1"}"
 
-  location = "${var.azure_location}"
-  name = "es-${var.es_cluster}-clients-lb"
+  location            = "${var.azure_location}"
+  name                = "es-${var.es_cluster}-clients-lb"
   resource_group_name = "${azurerm_resource_group.elasticsearch.name}"
 
   frontend_ip_configuration {
-    name = "es-${var.es_cluster}-ip"
-    subnet_id = "${azurerm_subnet.elasticsearch_subnet.id}"
+    name                          = "es-${var.es_cluster}-ip"
+    subnet_id                     = "${azurerm_subnet.elasticsearch_subnet.id}"
     private_ip_address_allocation = "dynamic"
   }
 }
@@ -23,8 +23,8 @@ resource "azurerm_lb" "clients" {
 resource "azurerm_lb" "clients-public" {
   count = "${var.associate_public_ip == "true" ? "1" : "0"}"
 
-  location = "${var.azure_location}"
-  name = "es-${var.es_cluster}-clients-public-lb"
+  location            = "${var.azure_location}"
+  name                = "es-${var.es_cluster}-clients-public-lb"
   resource_group_name = "${azurerm_resource_group.elasticsearch.name}"
 
   frontend_ip_configuration {
@@ -34,52 +34,52 @@ resource "azurerm_lb" "clients-public" {
 }
 
 resource "azurerm_lb_backend_address_pool" "clients-lb-backend" {
-  name = "es-${var.es_cluster}-clients-lb-backend"
+  name                = "es-${var.es_cluster}-clients-lb-backend"
   resource_group_name = "${azurerm_resource_group.elasticsearch.name}"
-  loadbalancer_id = "${var.associate_public_ip == true ? azurerm_lb.clients-public.id : azurerm_lb.clients.id}"
+  loadbalancer_id     = "${var.associate_public_ip == true ? azurerm_lb.clients-public.id : azurerm_lb.clients.id}"
 }
 
 resource "azurerm_lb_probe" "clients-httpprobe" {
-  name = "es-${var.es_cluster}-clients-lb-probe"
-  port = 8080
-  protocol = "Http"
-  request_path = "/status"
+  name                = "es-${var.es_cluster}-clients-lb-probe"
+  port                = 8080
+  protocol            = "Http"
+  request_path        = "/status"
   resource_group_name = "${azurerm_resource_group.elasticsearch.name}"
-  loadbalancer_id = "${var.associate_public_ip == true ? azurerm_lb.clients-public.id : azurerm_lb.clients.id}"
+  loadbalancer_id     = "${var.associate_public_ip == true ? azurerm_lb.clients-public.id : azurerm_lb.clients.id}"
 }
 
 // Kibana and Elasticsearch access - protected by default by the nginx proxy
 resource "azurerm_lb_rule" "clients-lb-rule" {
-  name = "es-${var.es_cluster}-clients-lb-rule"
-  backend_port = 8080
-  frontend_port = 80
+  name                           = "es-${var.es_cluster}-clients-lb-rule"
+  backend_port                   = 8080
+  frontend_port                  = 80
   frontend_ip_configuration_name = "${var.associate_public_ip == true ? "es-${var.es_cluster}-public-ip" : "es-${var.es_cluster}-ip"}"
-  backend_address_pool_id = "${azurerm_lb_backend_address_pool.clients-lb-backend.id}"
-  protocol = "Tcp"
-  loadbalancer_id = "${var.associate_public_ip == true ? azurerm_lb.clients-public.id : azurerm_lb.clients.id}"
-  resource_group_name = "${azurerm_resource_group.elasticsearch.name}"
+  backend_address_pool_id        = "${azurerm_lb_backend_address_pool.clients-lb-backend.id}"
+  protocol                       = "Tcp"
+  loadbalancer_id                = "${var.associate_public_ip == true ? azurerm_lb.clients-public.id : azurerm_lb.clients.id}"
+  resource_group_name            = "${azurerm_resource_group.elasticsearch.name}"
 }
 
 // Grafana instance, protected by default by their own login screen
 resource "azurerm_lb_rule" "clients-lb-rule2" {
-  name = "es-${var.es_cluster}-clients-lb-rule2"
-  backend_port = 3000
-  frontend_port = 3000
+  name                           = "es-${var.es_cluster}-clients-lb-rule2"
+  backend_port                   = 3000
+  frontend_port                  = 3000
   frontend_ip_configuration_name = "${var.associate_public_ip == true ? "es-${var.es_cluster}-public-ip" : "es-${var.es_cluster}-ip"}"
-  backend_address_pool_id = "${azurerm_lb_backend_address_pool.clients-lb-backend.id}"
-  protocol = "Tcp"
-  loadbalancer_id = "${var.associate_public_ip == true ? azurerm_lb.clients-public.id : azurerm_lb.clients.id}"
-  resource_group_name = "${azurerm_resource_group.elasticsearch.name}"
+  backend_address_pool_id        = "${azurerm_lb_backend_address_pool.clients-lb-backend.id}"
+  protocol                       = "Tcp"
+  loadbalancer_id                = "${var.associate_public_ip == true ? azurerm_lb.clients-public.id : azurerm_lb.clients.id}"
+  resource_group_name            = "${azurerm_resource_group.elasticsearch.name}"
 }
 
 // SSH access
 resource "azurerm_lb_rule" "clients-lb-rule-ssh" {
-  name = "es-${var.es_cluster}-clients-lb-rule-ssh"
-  backend_port = 22
-  frontend_port = 22
+  name                           = "es-${var.es_cluster}-clients-lb-rule-ssh"
+  backend_port                   = 22
+  frontend_port                  = 22
   frontend_ip_configuration_name = "${var.associate_public_ip == true ? "es-${var.es_cluster}-public-ip" : "es-${var.es_cluster}-ip"}"
-  backend_address_pool_id = "${azurerm_lb_backend_address_pool.clients-lb-backend.id}"
-  protocol = "Tcp"
-  loadbalancer_id = "${var.associate_public_ip == true ? azurerm_lb.clients-public.id : azurerm_lb.clients.id}"
-  resource_group_name = "${azurerm_resource_group.elasticsearch.name}"
+  backend_address_pool_id        = "${azurerm_lb_backend_address_pool.clients-lb-backend.id}"
+  protocol                       = "Tcp"
+  loadbalancer_id                = "${var.associate_public_ip == true ? azurerm_lb.clients-public.id : azurerm_lb.clients.id}"
+  resource_group_name            = "${azurerm_resource_group.elasticsearch.name}"
 }
--- terraform-azure/variables.tf
+++ terraform-azure/variables.tf
@@ -1,5 +1,5 @@
 variable "azure_location" {
-  type = "string"
+  type    = "string"
   default = "East US"
 }
 
@@ -21,12 +21,12 @@ variable "azure_tenant_id" {
 
 variable "es_cluster" {
   description = "Name of the elasticsearch cluster, used in node discovery"
-  default = "my-cluster"
+  default     = "my-cluster"
 }
 
 variable "key_path" {
   description = "Key name to be used with the launched EC2 instances."
-  default = "~/.ssh/id_rsa.pub"
+  default     = "~/.ssh/id_rsa.pub"
 }
 
 variable "environment" {
@@ -34,23 +34,23 @@ variable "environment" {
 }
 
 variable "data_instance_type" {
-  type = "string"
+  type    = "string"
   default = "Standard_D12_v2"
 }
 
 variable "master_instance_type" {
-  type = "string"
+  type    = "string"
   default = "Standard_A2_v2"
 }
 
 variable "client_instance_type" {
-  type = "string"
+  type    = "string"
   default = "Standard_A2_v2"
 }
 
 variable "elasticsearch_volume_size" {
-  type = "string"
-  default = "100" # gb
+  type    = "string"
+  default = "100"    # gb
 }
 
 variable "use_instance_storage" {
@@ -71,12 +71,12 @@ variable "elasticsearch_logs_dir" {
 
 # default elasticsearch heap size
 variable "data_heap_size" {
-  type = "string"
+  type    = "string"
   default = "7g"
 }
 
 variable "master_heap_size" {
-  type = "string"
+  type    = "string"
   default = "2g"
 }
 
